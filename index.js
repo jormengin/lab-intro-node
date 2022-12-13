f20bfa4a386424d2034e8c89d2b817b7e9d15ded
Jorge Mendez <jorge.mendezgines@gmail.com>
2022-12-13 20:18:19 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
 
-  get(pos) {}
+    // Ordena el arreglo de items en orden ascendente
+    this.items.sort((a, b) => {
+      return a - b;
+    });
 
-  max() {}
+    // Actualiza la propiedad length del arreglo de items
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (this.items.indexOf(pos)===-1) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  sum() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+  }
 
-  avg() {}
+  min() {
+     if (this.items.length === 0) {
+       throw new Error("EmptySortedList");
+     }
+     return Math.min(...this.items);
+  }
+
+  sum() {
+    let sum = this.items.reduce((pv, cv) => (pv + cv), 0);
+    return sum;
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    let sum = this.items.reduce((pv, cv) => (pv += cv), 0);
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
